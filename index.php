7c8d8aca4ab238860a5f0ea993f9caecd9d45909
riza <146099417+Faturz@users.noreply.github.com>
2024-03-06 21:50:09 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="../styles/main.css">
+    <script src="../main.js"></script>
+    <title>Website BK</title>
+</head>
+<body>
+    <div class="header">
+        <div class="title">
+            <h1>Website Bk</h1>
+        </div>
+        <div class="profileNav">
+            <div class="photoProfile"></div>
+            <h3>Hello</h3>
+        </div>
+        <div class="nav">
+        <div class="navWrap">
+            <a href="home.php">
+                <span class="material-symbols-outlined">Home</span>
+                    Beranda
+            </a>
+            <div class="siswaNav">
+                <span class="material-symbols-outlined">person</span>
+                    Siswa
+                <div class="navList">
+                    <a href="siswa.php">Profil Siswa</a>
+                    <a href="#">Perencanaan Karir</a>
+                    <a href="#">Siswa</a>
+                </div>
+            </div>
+                <a href="#">
+                    <span class="material-symbols-outlined">settings</span>        
+                    Pengaturan
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
